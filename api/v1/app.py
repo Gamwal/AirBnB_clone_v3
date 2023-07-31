3e284be137840dcb0223aae76272d2e536b1f3f9
dkamundi <davekamundi@gmail.com>
2023-07-31 14:02:57 -0700

    Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,19 +1,31 @@
 #!/usr/bin/python3
 """ """
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
 import api.v1.views.index
 
 
+# Create the Flask app
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def teardown(exception):
+    """
+    Teardown function to close the database connection after each request.
+
+    Args:
+        exception: An exception object if there was an exception during the request.
+    """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
