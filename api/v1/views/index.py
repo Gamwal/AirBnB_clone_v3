64900760fb16c4008ee70a5f5c62aab3d58b2d0d
dkamundi <davekamundi@gmail.com>
2023-07-31 13:51:39 -0700

    Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,11 +3,17 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
-def status():
-    status_data = {
-            'status': 'OK'
+def stats():
+    stats = {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User"),
             }
-    return jsonify(status_data)
+    return jsonify(stats)
